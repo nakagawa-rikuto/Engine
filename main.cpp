3f933f6c593df08715b38a1ffd51c0bbacdb1b74
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2024-06-21 21:53:13 +0900

    06/21 Windowの表示

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <Windows.h>
+#include "System.h"
+
+const wchar_t kWindowTitle[] = L"Engine";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// Systemの初期化
+	System::Initialize(kWindowTitle, 1280, 720);
+
 	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
 
+	
+	// ウィンドウのxボタンが押されるまでループ
+	while (System::ProcessMessage() == 0) {
+
+		/* ////////////////////////////
+				　　ゲームの処理
+		*/ ////////////////////////////
+	}
+
 	return 0;
+
 }
