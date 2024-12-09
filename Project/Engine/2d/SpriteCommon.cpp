0575376f03442cbc2d813d25c17bd2908bd7316b
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2024-12-09 11:54:14 +0900

    commonの変更

--- Project/Engine/2d/SpriteCommon.cpp
+++ Project/Engine/2d/SpriteCommon.cpp
@@ -17,12 +17,12 @@ SpriteCommon::~SpriteCommon() {
 ///-------------------------------------------/// 
 /// VertexBufferの初期化
 ///-------------------------------------------///
-void SpriteCommon::VertexInitialize(ID3D12Device* device, uint32_t size, VertexData2D* data) {
+void SpriteCommon::VertexInitialize(ID3D12Device* device, uint32_t size) {
 	// 生成
 	vertex_ = std::make_unique<VertexBuffer2D>();
 	// buffer
 	vertex_->Create(device, sizeof(VertexData2D) * size);
-	vertex_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&data));
+	vertex_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
 	// view
 	vertexBufferView_.BufferLocation = vertex_->GetBuffer()->GetGPUVirtualAddress(); // 先頭アドレスから使用
 	vertexBufferView_.SizeInBytes = sizeof(VertexData2D) * size; // 使用するサイズ（頂点6つ分）
@@ -32,12 +32,12 @@ void SpriteCommon::VertexInitialize(ID3D12Device* device, uint32_t size, VertexD
 ///-------------------------------------------/// 
 /// IndexBufferの初期化
 ///-------------------------------------------///
-void SpriteCommon::IndexInitialize(ID3D12Device* device, uint32_t size, uint32_t* data) {
+void SpriteCommon::IndexInitialize(ID3D12Device* device, uint32_t size) {
 	// 生成
 	index_ = std::make_unique<IndexBuffer2D>();
 	// buffer
 	index_->Create(device, sizeof(uint32_t) * size);
-	index_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&data));
+	index_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&indexData_));
 	// view
 	indexBufferView_.BufferLocation = index_->GetBuffer()->GetGPUVirtualAddress(); // 先頭のアドレスから使用
 	indexBufferView_.SizeInBytes = sizeof(uint32_t) * size; // 使用するサイズ（６つ分）
@@ -47,23 +47,23 @@ void SpriteCommon::IndexInitialize(ID3D12Device* device, uint32_t size, uint32_t
 ///-------------------------------------------/// 
 /// MaterialBufferの初期化
 ///-------------------------------------------///
-void SpriteCommon::MaterialInitialize(ID3D12Device* device, uint32_t size, MaterialData2D* data) {
+void SpriteCommon::MaterialInitialize(ID3D12Device* device, uint32_t size) {
 	// 生成
 	material_ = std::make_unique<Material2D>();
 	// buffer
 	material_->Create(device, sizeof(MaterialData2D) * size);
-	material_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&data));
+	material_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
 }
 
 ///-------------------------------------------/// 
 /// WVPBufferの初期化
 ///-------------------------------------------///
-void SpriteCommon::WVPMatrixInitialize(ID3D12Device* device, TransformationMatrix2D* data) {
+void SpriteCommon::WVPMatrixInitialize(ID3D12Device* device) {
 	// 生成
 	wvp_ = std::make_unique<Transform2D>();
 	// buffer
 	wvp_->Create(device, sizeof(TransformationMatrix2D));
-	wvp_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&data));
+	wvp_->GetBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&wvpMatrixData_));
 }
 
 ///-------------------------------------------/// 
@@ -81,4 +81,14 @@ void SpriteCommon::Bind(ID3D12GraphicsCommandList * commandList, BlendMode mode)
 	commandList->SetGraphicsRootConstantBufferView(0, material_->GetBuffer()->GetGPUVirtualAddress());
 	// wvpMatrixBufferの設定
 	commandList->SetGraphicsRootConstantBufferView(1, wvp_->GetBuffer()->GetGPUVirtualAddress());
+}
+
+///-------------------------------------------/// 
+/// Dataの設定
+///-------------------------------------------///
+void SpriteCommon::SetData(VertexData2D* vertex, uint32_t* index, MaterialData2D* materal, TransformationMatrix2D* wvp) {
+	vertexData_ = vertex;
+	indexData_ = index;
+	materialData_ = materal;
+	wvpMatrixData_ = wvp;
 }
