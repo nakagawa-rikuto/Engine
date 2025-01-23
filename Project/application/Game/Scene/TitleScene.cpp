a4e10fca94e528065de44dc166af7ef889e06e92
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2025-01-23 16:37:42 +0900

    タイトル

--- Project/application/Game/Scene/TitleScene.cpp
+++ Project/application/Game/Scene/TitleScene.cpp
@@ -2,31 +2,59 @@
 // SceneManager
 #include "application/Manager/SceneManager.h"
 
-///-------------------------------------------/// 
+///-------------------------------------------///
 /// デストラクタ
 ///-------------------------------------------///
 TitleScene::~TitleScene() {}
 
-///-------------------------------------------/// 
+///-------------------------------------------///
 /// 初期化
 ///-------------------------------------------///
 void TitleScene::Initialize() {
 	// ISceneの初期化(デフォルトカメラとカメラマネージャ)
 	IScene::Initialize();
+
+	Loader_->LoadModel("title");
+
+	titleModel_ = std::make_unique<Model>();
+	titleModel_->Initialize("title");
+
+	/// ===Camera=== ///
+	// Camera情報
+	cameraPos_ = {3.0f, 0.0f, -10.0f};
+	cameraRotate_ = {0.0f, 0.0f, 0.0f};
+	cameraScale_ = {0.0f, 0.0f, 0.0f};
+
+	camera_ = std::make_shared<Camera>();
+	camera_->Initialize();
+	camera_->SetTranslate(cameraPos_);
+	cameraManager_->Add("main2", camera_);
 }
 
-///-------------------------------------------/// 
+///-------------------------------------------///
 /// 更新
 ///-------------------------------------------///
 void TitleScene::Update() {
 	/// ===デバック用ImGui=== ///
 #ifdef USE_IMGUI
 	ImGui::Begin("TitleScene");
+
+	ImGui::Begin("Camera");
+	ImGui::DragFloat3("pos", &cameraPos_.x, 0.01f);
+	ImGui::End();
+
 	ImGui::End();
 #endif // USE_IMGUI
+
+	cameraManager_->SetActiveCamera("main2");
+	camera_->SetTranslate(cameraPos_);
+	cameraManager_->UpdateAllCameras();
+
+	titleModel_->Update();
+	titleModel_->SetCamera(cameraManager_->GetActiveCamera().get());
 }
 
-///-------------------------------------------/// 
+///-------------------------------------------///
 /// 描画
 ///-------------------------------------------///
 void TitleScene::Draw() {
@@ -34,6 +62,9 @@ void TitleScene::Draw() {
 #pragma endregion
 
 #pragma region モデル描画
+
+	titleModel_->Draw();
+
 #pragma endregion
 
 #pragma region 前景スプライト描画
--- Project/application/Game/Scene/TitleScene.h
+++ Project/application/Game/Scene/TitleScene.h
@@ -1,14 +1,17 @@
 #pragma once
 /// ===Include=== ///
 // IScene
+#include "application/3d/Camera.h"
+#include "application/3d/Model.h"
 #include "application/Game/Scene/IScene.h"
 
-///=====================================================/// 
+#include <memory>
+
+///=====================================================///
 /// タイトルシーン
 ///=====================================================///
 class TitleScene : public IScene {
-public:/// ===メンバ関数=== ///
-
+public: /// ===メンバ関数=== ///
 	TitleScene() = default;
 	~TitleScene();
 
@@ -19,9 +22,18 @@ public:/// ===メンバ関数=== ///
 	// 描画
 	void Draw() override;
 
-private:/// ===メンバ変数=== ///
+private: /// ===メンバ変数=== ///
 	/// <summary>
 	/// シーン用
 	/// </summary>
-};
+	
+	/// ===Camera=== ///
+	std::shared_ptr<Camera> camera_;
+	// Camera情報
+	Vector3 cameraPos_;
+	Vector3 cameraRotate_;
+	Vector3 cameraScale_;
 
+	// タイトル文字モデル
+	std::unique_ptr<Model> titleModel_ = nullptr;
+};
