b7096b71d02a3e51cdbf3414438701b44f4a0098
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2025-01-24 02:22:53 +0900

    AABBの追加

--- Project/application/Game/Objects/Collider.cpp
+++ Project/application/Game/Objects/Collider.cpp
@@ -1,1 +1,12 @@
 #include "Collider.h"
+
+void Collider::Initialize(Vector3 radius)
+{
+	this->radius = radius;
+}
+
+void Collider::Update(Vector3 center)
+{
+	aabb.min = center - radius;
+	aabb.max = center + radius;
+}
--- Project/application/Game/Objects/Collider.h
+++ Project/application/Game/Objects/Collider.h
@@ -1,8 +1,20 @@
 #pragma once
-
+#include "Math/MathData.h"
 
 class Collider
 {
 public:
+	
+	void Initialize(Vector3 radius);
+
+	void Update(Vector3 center);
+
+	AABB GetAABB() { return aabb; }
+
+private:
+
+	Vector3 radius;
+
+	AABB aabb;
 };
 
