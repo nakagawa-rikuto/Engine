6f36dd42177ee47323f2bbee5588b6a901984d57
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2025-01-26 18:57:45 +0900

    01/26

--- Project/application/Game/Scene/GameScene.cpp
+++ Project/application/Game/Scene/GameScene.cpp
@@ -1,11 +1,15 @@
 #include "GameScene.h"
 // SceneManager
 #include "application/Manager/SceneManager.h"
+// Engine
+#include "Engine/Service/Loader.h"
 
 ///-------------------------------------------/// 
 /// デストラクタ
 ///-------------------------------------------///
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	model_.reset();
+}
 
 ///-------------------------------------------/// 
 /// 初期化
@@ -13,6 +17,19 @@ GameScene::~GameScene() {}
 void GameScene::Initialize() {
 	// ISceneの初期化(デフォルトカメラとカメラマネージャ)
 	IScene::Initialize();
+
+	// モデルの読み込み
+	Loader::LoadModel("MonsterBall");
+
+	// モデルの生成
+	model_ = std::make_unique<Model>();
+	model_->Initialize("MonsterBall", LightType::HalfLambert);
+
+	// Light
+	lightDirection_ = { 0.0f, -1.0f, 0.0f };
+	lightIntensity_ = 1.0f;
+	lightColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+	lightShininess_ = 48.5f;
 }
 
 ///-------------------------------------------/// 
@@ -22,8 +39,31 @@ void GameScene::Update() {
 	/// ===デバック用ImGui=== ///
 #ifdef USE_IMGUI
 	ImGui::Begin("GameScene");
+
+	// Model
+	ImGui::DragFloat3("Tranlate", &modelTranslate_.x, 0.1f);
+	ImGui::DragFloat3("Rotate", &modelRotate_.x, 0.1f);
+	ImGui::DragFloat3("Size", &modelScale_.x, 0.1f);
+	ImGui::ColorEdit4("Color", &modelColor_.x);
+	// Light
+	ImGui::DragFloat3("LightDirection", &lightDirection_.x, 0.01f);
+	ImGui::DragFloat("lightIntensity", &lightIntensity_, 0.01f);
+	ImGui::DragFloat("LightShininess", &lightShininess_, 0.01f);
+	ImGui::ColorEdit4("LigthColor", &lightColor_.x);
+
 	ImGui::End();
 #endif // USE_IMGUI
+
+	/// ===モデルの更新=== ///
+	model_->SetPosition(modelTranslate_);
+	model_->SetRotate(modelRotate_);
+	model_->SetScale(modelScale_);
+	model_->SetColor(modelColor_);
+	model_->SetLightColor(lightColor_);
+	model_->SetLightDirection(lightDirection_);
+	model_->SetLightIntensity(lightIntensity_);
+	model_->SetLightShininess(lightShininess_);
+	model_->Update();
 }
 
 ///-------------------------------------------/// 
@@ -34,6 +74,7 @@ void GameScene::Draw() {
 #pragma endregion
 
 #pragma region モデル描画
+	model_->Draw();
 #pragma endregion
 
 #pragma region 前景スプライト描画
--- Project/application/Game/Scene/GameScene.h
+++ Project/application/Game/Scene/GameScene.h
@@ -1,6 +1,7 @@
 #pragma once
 /// ===Includ=== ///
 #include "application/Game/Scene/IScene.h"
+#include "application/3d/Model.h"
 
 ///=====================================================/// 
 /// ゲームシーン
@@ -22,5 +23,20 @@ private:/// ===メンバ変数=== ///
 	/// <summary>
 	/// シーン用
 	/// </summary>
+
+	// モデル
+	std::unique_ptr<Model> model_;
+	/// ===変数=== ///
+	// モデル
+	Vector3 modelTranslate_ = { 0.0f, 0.0f, 0.0f };
+	Vector3 modelRotate_ = { 0.0f, 0.0f, 0.0f };
+	Vector3 modelScale_ = { 1.0f, 1.0f, 1.0f };
+	Vector4 modelColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+	// ライト
+	Vector3 lightDirection_ = { 0.0f, 1.0f, 0.0f };
+	float lightIntensity_ = 1.0f;
+	Vector4 lightColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+	float lightShininess_ = 48.5f;
+	
 };
 
