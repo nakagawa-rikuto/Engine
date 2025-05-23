12bfc0cddda5891643af7f836925034e62a6da24
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2025-05-23 21:00:41 +0900

    05/23

--- Project/Editor/level_editor.py
+++ Project/Editor/level_editor.py
@@ -1,7 +1,9 @@
 import bpy # type: ignore
-import math
 import bpy_extras # type: ignore
-
+import gpu # type: ignore
+import gpu_extras.batch # type: ignore
+import math
+import copy
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -32,14 +34,18 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    # メニューに項目を追加（変更）
+    # メニューに項目を追加
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    # 3Dビューに描画関数を追加
+    DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(DrawCollider.draw_collider, (), "WINDOW", "POST_VIEW")
     print("レベルエディタが有効化されました。")
 
 # Add-On無効化時コールバック
 def unregister():
-    # メニューから項目を削除（変更）
+    # メニューから項目を削除
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    # 3Dビューから描画関数を削除
+    bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle, "WINDOW")
 
     # Blenderからクラスを削除
     for cls in classes:
@@ -47,7 +53,7 @@ def unregister():
     print("レベルエディタが無効化されました。")
 
 
-    # トップバーの拡張メニュー
+# トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     # Blenderがクラスを識別する為の固有の文字列
     bl_idname = "TOPBAR_MT_my_menu"
@@ -205,7 +211,8 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         self.report({'INFO'}, "シーン情報をExportしました")
         print("シーン情報をExportしました")
         return {'FINISHED'}
-    
+
+
 # パネル　ファイル名
 class OBJECT_PT_file_name(bpy.types.Panel):
     """オブジェクトのファイルネームパネル"""
@@ -226,6 +233,7 @@ class OBJECT_PT_file_name(bpy.types.Panel):
            # プロパティがなければ、プロパティ追加ボタンを表示\
            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
 
+
 # オペレータ カスタムプロパティ「file_name」追加
 class MYADDON_OT_add_filename(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_add_filename"
@@ -240,6 +248,7 @@ class MYADDON_OT_add_filename(bpy.types.Operator):
 
         return {'FINISHED'}
 
+
 # Blenderに登録するクラスリスト
 classes = (
     # オペレータを追加するたびにここに追加
@@ -250,3 +259,81 @@ classes = (
     OBJECT_PT_file_name,
     MYADDON_OT_add_filename,
 )
+
+
+# コライダー描画
+class DrawCollider:
+
+    # 描画ハンドル
+    handle = None
+
+    # 3Dビューに登録する描画関数
+    def draw_collider():
+        #頂点データ
+        vertices = {"pos": []}
+        # インデックスデータ
+        indices = []
+
+        # 各頂点の、オブジェクト中心からオフセット
+        offsets = [
+            [-0.5, -0.5, -0.5],  # 左下前
+            [+0.5, -0.5, -0.5],  # 右下前
+            [-0.5, +0.5, -0.5],  # 左上前
+            [+0.5, +0.5, -0.5],  # 右上前
+            [-0.5, -0.5, +0.5],  # 左下奥
+            [+0.5, -0.5, +0.5],  # 右下奥
+            [-0.5, +0.5, +0.5],  # 左上奥
+            [+0.5, +0.5, +0.5],  # 右上奥
+        ]
+
+        # 立方体のX, Y, Z方向のサイズ
+        size = [2,2,2]
+
+        # シーンのオブジェクトリストを走査
+        for object in bpy.context.scene.objects:
+            # 追加前の頂点数を記録
+            start = len(vertices["pos"])
+
+            # Boxの8頂点を処理
+            for offset in offsets:
+                # オブジェクトの中心座標をコピー
+                pos = copy.copy(object.location)
+                # 各軸方向にサイズとオフセットを適用
+                pos[0] += offset[0] * size[0]
+                pos[1] += offset[1] * size[1]
+                pos[2] += offset[2] * size[2]
+                # 頂点データとして追加
+                vertices["pos"].append(pos)
+
+                # 各面を構成する辺のインデックス
+                # 前面（Zマイナス側）
+                indices.append([start+0, start+1])
+                indices.append([start+2, start+3])
+                indices.append([start+0, start+2])
+                indices.append([start+1, start+3])
+
+                # 背面（Zプラス側）
+                indices.append([start+4, start+5])
+                indices.append([start+6, start+7])
+                indices.append([start+4, start+6])
+                indices.append([start+5, start+7])
+
+                # 前後をつなぐ辺
+                indices.append([start+0, start+4])
+                indices.append([start+1, start+5])
+                indices.append([start+2, start+6])
+                indices.append([start+3, start+7])
+
+        # ビルトインのシェーダーを取得
+        # 3D_UNIFORM_COLORはない
+        shader = gpu.shader.from_builtin("UNIFORM_COLOR")
+
+        # バッチを作成（引数 : シェーダー、 トポロジー、頂点データ、　インデックスデータ）
+        batch = gpu_extras.batch.batch_for_shader(shader, "LINES", vertices, indices = indices)
+
+        # シェーダーのパラメーター設定
+        color = [0.5, 1.0, 1.0, 1.0]
+        shader.bind()
+        shader.uniform_float("color", color)
+        # 描画
+        batch.draw(shader)
