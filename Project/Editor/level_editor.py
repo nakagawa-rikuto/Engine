c147bd8d3f195831d136988f9d244fcac9155a22
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2025-05-23 13:07:47 +0900

    05/09

--- Project/Editor/level_editor.py
+++ Project/Editor/level_editor.py
@@ -14,14 +14,65 @@ bl_info = {
     "category": "Object"
 }
 
-# アドオン有効化時コールバック
+
+# メニュー項目描画
+def draw_menu_manual(self, context):
+    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
+    #context : カーソルを合わせた時のポップアップのカスタマイズなどに使用
+
+    # トップバーの「エディターメニュー」に項目（オペレータ）を追加
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+# Add-On有効化時コールバック
 def register():
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # メニューに項目を追加（変更）
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
-# アドオン無効化時コールバック
+# Add-On無効化時コールバック
 def unregister():
+    # メニューから項目を削除（変更）
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    # Blenderからクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
+
+
+    # トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    # Blenderがクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    # メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+    # 説明表示用の文字列
+    bl_description = "拡張メニュー by " + __name__
+
+    # サブメニューの描画
+    def draw(self, context):
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
     
-# テスト実行用コード
-if __name__ == "__main__":
-    register()
+        # 区切り線
+        self.layout.separator()
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+        # 区切り線
+        self.layout.separator()
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+    # 既存のメニューにサブメニューを追加
+    def submenu(self, context):
+        # ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+
+# Blenderに登録するクラスリスト
+classes = (
+    TOPBAR_MT_my_menu,
+)
